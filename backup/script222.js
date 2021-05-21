798f883bd20d722e35b1f253b8c5a5e60a1149e6
Scrappy <danielyalta@yandex.ru>
2021-05-21 22:16:26 +0300

    first

---
+++ backup/script222.js
@@ -0,0 +1,118 @@
+
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Contact Form
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+        formData.append('image', formImage.files[0]);
+
+        // Если ошибок нет, то отправляем
+        if (error === 0) {
+            form.classList.add('_sending');
+
+            // Fetch конструкция для отправки на почту:
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+            } else {
+                alert('Ошибка');
+            }
+
+        } else {
+            alert('Заполните поля');
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    // Функция теста email
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+
+    // Функция теста телефон
+    function phoneTest(input) {
+        return /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
+    }
+
+
+    // Получаем инпут file в переменную
+
+    const formImage = document.getElementById('formImage');
+    const formPreview = document.getElementById('formPreview');
+
+    formImage.addEventListener('change', () => {
+        uploadFile(formImage.files[0]);
+    });
+
+    function uploadFile(file) {
+        // проверяем тип файла
+        if (!['image/jpeg', 'image/jpg', 'image/png', 'image/gif'].includes(file.type)) {
+            alert('Разрешены только изображения.');
+            formImage.value = '';
+            return;
+        }
+        // проверим размер файла (<10 мб)
+        if (file.size > 10 * 1024 * 1024) {
+            alert('Файл должен быть менее 10 МБ.');
+            return;
+        }
+
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+        };
+        reader.onerror = function (e) {
+            alert('Ошибка');
+        };
+        reader.readAsDataURL(file);
+    }
+
+});
