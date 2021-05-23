daa5aaa1521f3d574ad4282e37bb4efafcc3c9bb
Vadim Shvetsov <vadim@shvetsov.io>
2021-05-23 19:38:44 +0300

    Add env vars

--- api/nodemailer.js
+++ api/nodemailer.js
@@ -1,38 +1,28 @@
 "use strict";
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "Yandex",
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+async function mail() {
+  const emailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "scrapwns@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  };
+
+  // 3. This will send the email with the `emailOptions` above.
+  return transporter.sendMail(emailOptions);
+}
+
 export default async function handler(req, res) {
-  let testAccount = await nodemailer.createTestAccount();
-
-  const transporter = nodemailer.createTransport({
-    // host: process.env.MAIL_HOST,
-    // port: 25,
-    // auth: {
-    // user: process.env.MAIL_USER,
-    // pass: process.env.MAIL_PASS,
-    // },
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
-
-  async function mail() {
-    const emailOptions = {
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "scrapwns@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    };
-
-    // 3. This will send the email with the `emailOptions` above.
-    return transporter.sendMail(emailOptions);
-  }
-
   if (req.method === "POST") {
     const emailRes = await mail(req.body);
     if (emailRes.messageId) {
--- js/script.js
+++ js/script.js
@@ -123,4 +123,3 @@ document.addEventListener("DOMContentLoaded", function () {
     reader.readAsDataURL(file);
   }
 });
-
