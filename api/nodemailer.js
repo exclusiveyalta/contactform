a6dad412e40282dc44bb3b5678f5edbb88ba094a
Vadim Shvetsov <vadim@shvetsov.io>
2021-05-23 20:28:50 +0300

    test another yandex conf

--- api/nodemailer.js
+++ api/nodemailer.js
@@ -1,32 +1,32 @@
 "use strict";
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "Yandex",
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+async function mail({ name, phone, email, message, image }) {
+  const emailOptions = {
+    from: '"S_crappy PWNS 👻" <scrapwns@yandex.ru>', // sender address
+    to: "scrapwns@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: `Hello world? Name = ${name}, phone = ${phone}, email = ${email}, message = ${message}`, // plain text
+    html: `<b>Hello world?</b> Name = ${name}, phone = ${phone}, email = ${email}, message = ${message}`, // html body
+  };
+
+  // 3. This will send the email with the `emailOptions` above.
+  return transporter.sendMail(emailOptions);
+}
+
 export default async function handler(req, res) {
-  const transporter = nodemailer.createTransport({
-    service: "Yandex",
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
-
-  console.log(process.env.MAIL_USER, process.env.MAIL_PASS);
-
-  async function mail({ name, phone, email, message, image }) {
-    const emailOptions = {
-      from: '"S_crappy PWNS 👻" <scrapwns@yandex.ru>', // sender address
-      to: "scrapwns@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: `Hello world? Name = ${name}, phone = ${phone}, email = ${email}, message = ${message}`, // plain text
-      html: `<b>Hello world?</b> Name = ${name}, phone = ${phone}, email = ${email}, message = ${message}`, // html body
-    };
-
-    // 3. This will send the email with the `emailOptions` above.
-    return transporter.sendMail(emailOptions);
-  }
-
   if (req.method === "POST") {
     const emailRes = await mail(req.body);
+    console.log(req.body, JSON.parse(req.body));
+
     if (emailRes.messageId) {
       return res.status(200).json({ message: `Email sent successfuly` });
     }
