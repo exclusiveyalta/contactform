9f676f94cbae0163338c59e78b5fb43ea7eb2edf
Vadim Shvetsov <vadim@shvetsov.io>
2021-05-23 20:04:46 +0300

    test another yandex conf

--- api/nodemailer.js
+++ api/nodemailer.js
@@ -2,9 +2,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.yandex.ru",
-  port: 465,
-  secure: true,
+  service: "Yandex",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
