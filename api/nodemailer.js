2c73f7ecf1e5edffee96f1c45b0b7b0002bf8f84
Vadim Shvetsov <vadim@shvetsov.io>
2021-05-23 20:10:26 +0300

    test another yandex conf

--- api/nodemailer.js
+++ api/nodemailer.js
@@ -1,30 +1,30 @@
 "use strict";
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "Yandex",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-
-console.log(process.env.MAIL_USER, process.env.MAIL_PASS);
-
-async function mail() {
-  const emailOptions = {
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "scrapwns@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  };
-
-  // 3. This will send the email with the `emailOptions` above.
-  return transporter.sendMail(emailOptions);
-}
-
 export default async function handler(req, res) {
+  const transporter = nodemailer.createTransport({
+    service: "Yandex",
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  console.log(process.env.MAIL_USER, process.env.MAIL_PASS);
+
+  async function mail() {
+    const emailOptions = {
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "scrapwns@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    };
+
+    // 3. This will send the email with the `emailOptions` above.
+    return transporter.sendMail(emailOptions);
+  }
+
   if (req.method === "POST") {
     const emailRes = await mail(req.body);
     if (emailRes.messageId) {
